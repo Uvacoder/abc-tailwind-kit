c0ca309a5c115a5c997108c3dfc395884a8a6455
uvacoder <uva22940coder@gmail.com>
2023-01-12 02:14:21 -0500

    Update index.tsx

--- pages/components/cta/index.tsx
+++ pages/components/cta/index.tsx
@@ -60,7 +60,6 @@ const ComponentsPage: FC = () => {
                 vertical={true}
                 showSwitchMode={true}
                 title="With image"
-                withPub
                 element={
                     <SimpleTextCta
                         isLeft={true}
@@ -106,7 +105,6 @@ const ComponentsPage: FC = () => {
                 vertical={true}
                 showSwitchMode={true}
                 title="Multiple images cta"
-                withPub
                 element={
                     <MultipleImagesCta
                         title="Mother hearth host your travel"
